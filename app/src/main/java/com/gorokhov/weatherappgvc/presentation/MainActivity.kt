c422341908a00e8f643ea95fe48d0963d8ffefb8
Anton <bonduelle85@gmail.com>
2025-12-24 19:27:26 +0300

    RootComponent

--- app/src/main/java/com/gorokhov/weatherappgvc/presentation/MainActivity.kt
+++ app/src/main/java/com/gorokhov/weatherappgvc/presentation/MainActivity.kt
@@ -5,32 +5,34 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.arkivanov.decompose.defaultComponentContext
+import com.gorokhov.weatherappgvc.WeatherApp
 import com.gorokhov.weatherappgvc.data.network.api.ApiFactory
 import com.gorokhov.weatherappgvc.data.network.api.ApiService
+import com.gorokhov.weatherappgvc.presentation.root.DefaultRootComponent
+import com.gorokhov.weatherappgvc.presentation.root.RootContent
 import com.gorokhov.weatherappgvc.presentation.ui.theme.WeatherAppGVCTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var rootComponentFactory: DefaultRootComponent.Factory
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        (applicationContext as WeatherApp).applicationComponent.inject(this)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val apiService = ApiFactory.apiService
-        CoroutineScope(Dispatchers.Main).launch {
-            val currentWeather = apiService.loadCurrentWeather("London")
-            val forecast = apiService.loadForecast("London")
-            val city = apiService.searchCity("London")
-            Log.d("MainActivity", "${currentWeather.weather}")
-            Log.d("MainActivity", "${forecast.forecastDto.forecastDay[0].dayWeatherDto}")
-            Log.d("MainActivity", city[0].name)
-        }
+        val component = rootComponentFactory.create(defaultComponentContext())
 
         setContent {
-            WeatherAppGVCTheme {
-
-            }
+            RootContent(component = component)
         }
     }
 }
--- app/src/main/java/com/gorokhov/weatherappgvc/presentation/root/RootContent.kt
+++ app/src/main/java/com/gorokhov/weatherappgvc/presentation/root/RootContent.kt
@@ -1,8 +1,30 @@
 package com.gorokhov.weatherappgvc.presentation.root
 
 import androidx.compose.runtime.Composable
+import com.arkivanov.decompose.extensions.compose.jetpack.stack.Children
+import com.gorokhov.weatherappgvc.presentation.details.DetailsContent
+import com.gorokhov.weatherappgvc.presentation.favourite.FavouriteContent
+import com.gorokhov.weatherappgvc.presentation.search.SearchContent
+import com.gorokhov.weatherappgvc.presentation.ui.theme.WeatherAppGVCTheme
 
 @Composable
 fun RootContent(component: RootComponent) {
-    
+
+    WeatherAppGVCTheme {
+        Children(
+            stack = component.stack
+        ) {
+            when(val instance = it.instance) {
+                is RootComponent.Child.Details -> {
+                    DetailsContent(component = instance.component)
+                }
+                is RootComponent.Child.Favourite -> {
+                    FavouriteContent(component = instance.component)
+                }
+                is RootComponent.Child.Search -> {
+                    SearchContent(component = instance.component)
+                }
+            }
+        }
+    }
 }
